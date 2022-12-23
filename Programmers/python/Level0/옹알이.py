9f73e31d07d1a4cc43c096a792f3e7243901d285
Sun Yerim <syl1602@naver.com>
2022-12-23 14:44:22 +0900

    [12-23] 옹알이.py

--- Programmers/python/Level0/옹알이.py
+++ Programmers/python/Level0/옹알이.py
@@ -1,12 +1,16 @@
+from itertools import permutations
+
 def solution(babbling):
     ong = ["aya", "ye", "woo", "ma"]
-    answer = 0;
-    for i in range (0, len(babbling)):
-        if babbling[i] in ong:
-            answer = answer + 1
+    johab = []
+    answer = 0
+    for i in range(1, len(ong)+1):
+        for j in permutations(ong, i):
+            johab.append(''.join(j))
+            
+    for i in babbling:
+        if i in johab:
+            answer += 1
+            
     return answer
-
-
-babbling = ["aya" "yee", "u", "maa", "wyeoo"]
-print(solution(babbling))
 
